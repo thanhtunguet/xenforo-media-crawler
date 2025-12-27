ee9755077959e646dc1b0e3da850ab6ab376241f
thanhtunguet <thanhtung.uet@gmail.com>
2025-12-28 00:21:46 +0700

    Enhance CI/CD workflow and frontend configuration
    - Updated GitHub Actions workflow to build and push separate Docker images for backend and frontend applications.
    - Modified Docker image tags to reflect application type (backend/frontend).
    - Added condition to skip Docker login during pull requests.
    - Enabled standalone output for Docker in the frontend Next.js configuration.

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -10,6 +10,8 @@ const nextConfig = {
   // Use this to set Nx-specific options
   // See: https://nx.dev/recipes/next/next-config-setup
   nx: {},
+  // Enable standalone output for Docker
+  output: 'standalone',
 };
 
 const plugins = [
