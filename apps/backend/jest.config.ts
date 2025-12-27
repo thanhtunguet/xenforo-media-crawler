af0267c85ceaa3b683d7881ed4187c9e4c6b73c5
thanhtunguet <thanhtung.uet@gmail.com>
2025-12-27 22:30:58 +0700

    Migrate to nx workspace

---
+++ apps/backend/jest.config.ts
@@ -0,0 +1,16 @@
+export default {
+  displayName: 'backend',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/backend',
+  testMatch: ['**/*.spec.ts'],
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1',
+  },
+  rootDir: '.',
+};
+
